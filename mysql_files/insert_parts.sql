e29d6dd4635184ad25ae7149e2966bb5300a8059
CharlesCoeder <croederdev@gmail.com>
2022-12-03 22:30:23 -0500

    added cpu data

---
+++ mysql_files/insert_parts.sql
@@ -0,0 +1,11 @@
+INSERT INTO cpu(item_id,item_cat,manufacturer,part_no,cores,clock_speed,boosted_clock_speed,tdp,series,microarchitecture,core_family,socket,integrated_graphics,max_mem,ecc,cooler_incl,packaging,lithography) VALUES
+ (9019,'cpu','Weber, Mertz and Lesch',895,26,1.5,1.8,85,'mi','nunc','dapibus','quis tortor','nisl',95,'0','1','Envelope',19)
+,(9020,'cpu','Sawayn-Beahan',643,32,1.3,1.56,65,'maecenas tincidunt','in','libero','eget','ac enim',121,'1','1','Boxed',18)
+,(9031,'cpu','Kilback-Gusikowski',543,3,1.5,1.8,65,'consequat morbi','sit','turpis','mi','tempor turpis',121,'0','1','Boxed',24)
+,(9040,'cpu','Doyle-Conroy',529,30,2.2,2.64,115,'ipsum aliquam','phasellus id','lectus in','quam suspendisse','vestibulum',66,'1','0','Boxed',3)
+,(9046,'cpu','Goldner-Dickinson',773,5,1.3,1.56,125,'suspendisse','porttitor','posuere','sed ante','duis',31,'1','1','Envelope',25)
+,(9047,'cpu','Senger-Jast',796,4,2.4,2.88,95,'porttitor id','quis','pede justo','nullam','ligula',97,'0','1','Boxed',13)
+,(9061,'cpu','Walsh, Jast and Smitham',739,3,2.1,2.52,95,'ante','leo','enim','sapien','duis',15,'0','1','Envelope',15)
+,(9064,'cpu','Orn and Sons',160,26,3.1,3.72,115,'vel','in purus','cursus urna','vel enim','justo',116,'0','0','Envelope',20)
+,(9065,'cpu','Purdy-Nader',869,19,2.5,3.0,125,'potenti nullam','pede ullamcorper','semper rutrum','at diam','pede posuere',40,'1','0','Envelope',13)
+,(9073,'cpu','Boyer, Romaguera and Rippin',809,12,2.2,2.64,115,'in ante','quam','vitae','nulla','ante ipsum',23,'1','0','Envelope',11);
