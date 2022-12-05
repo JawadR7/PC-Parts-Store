5cfcb42c75ae2d85647918e775d5554c8417978e
omarrris <100310239+omarrris@users.noreply.github.com>
2022-12-04 22:22:39 -0500

    removed incorrect views

--- mysql_files/create_db_views.sql
+++ mysql_files/create_db_views.sql
@@ -38,22 +38,3 @@ AS SELECT v.*, i.item_id AS base_item_id, i.item_name, i.vendor_id, i.item_desc,
 FROM item i INNER JOIN video_card v
 ON (i.item_id = v.item_id);
 
-
-CREATE VIEW CustomerView AS
-SELECT first_name, last_name, customer_phone, email 
-FROM customer AS c
-INNER JOIN item as i
-	ON c.item_name = i.item_name AND c.item_price = i.item_price 
-INNER JOIN address as a
-	ON c.street_number = a.street_number AND c.apt_no = a.apt_no AND c.street_name = a.street_name AND c.city = a.city AND c.country_id = a.country_id;
-
-    
-CREATE VIEW VendorView AS
-SELECT first_name, last_name, customer_phone, email 
-FROM customer AS c
-INNER JOIN item as i
-	ON c.item_id = i.item_id AND c.item_name = i.item_name AND c.item_price = i.item_price 
-INNER JOIN address as a
-	ON c.street_number = a.street_number AND c.apt_no = a.apt_no AND c.street_name = a.street_name AND c.city = a.city AND c.country_id = a.country_id
-INNER JOIN shipping_method s
-	ON c.method_name = s.method_name
