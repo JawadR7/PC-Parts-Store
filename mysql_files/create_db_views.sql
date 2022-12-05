549fa26791a6dc6ddee3a474e822c3c8bca7a0b0
CharlesCoeder <croederdev@gmail.com>
2022-12-04 22:00:41 -0500

    changed view names to avoid conflicts

--- mysql_files/create_db_views.sql
+++ mysql_files/create_db_views.sql
@@ -1,39 +1,39 @@
-CREATE VIEW CPUs 
+CREATE VIEW CPU_VIEW 
 AS SELECT c.*, i.item_id AS base_item_id, i.item_name, i.vendor_id, i.item_desc, i.item_cat AS base_item_cat, i.item_price 
 FROM item i INNER JOIN cpu c
 ON (i.item_id = c.item_id);
 
-CREATE VIEW POWERSUPPLY 
+CREATE VIEW POWERSUPPLY_VIEW 
 AS SELECT p.*, i.item_id AS base_item_id, i.item_name, i.vendor_id, i.item_desc, i.item_cat AS base_item_cat, i.item_price 
 FROM item i INNER JOIN power_supply p
 ON (i.item_id = p.item_id);
 
-CREATE VIEW MEMORY 
+CREATE VIEW MEMORY_VIEW 
 AS SELECT m.*, i.item_id AS base_item_id, i.item_name, i.vendor_id, i.item_desc, i.item_cat AS base_item_cat, i.item_price 
 FROM item i INNER JOIN memory m
 ON (i.item_id = m.item_id);
 
-CREATE VIEW CPUCOOLER 
+CREATE VIEW CPUCOOLER_VIEW 
 AS SELECT o.*, i.item_id AS base_item_id, i.item_name, i.vendor_id, i.item_desc, i.item_cat AS base_item_cat, i.item_price 
 FROM item i INNER JOIN cpu_cooler o
 ON (i.item_id = o.item_id);
 
-CREATE VIEW STORAGE 
+CREATE VIEW STORAGE_VIEW 
 AS SELECT s.*, i.item_id AS base_item_id, i.item_name, i.vendor_id, i.item_desc, i.item_cat AS base_item_cat, i.item_price 
 FROM item i INNER JOIN storage s
 ON (i.item_id = s.item_id);
 
-CREATE VIEW PCCASE 
+CREATE VIEW PCCASE_VIEW 
 AS SELECT r.*, i.item_id AS base_item_id, i.item_name, i.vendor_id, i.item_desc, i.item_cat AS base_item_cat, i.item_price 
 FROM item i INNER JOIN pc_case r
-ON (i.item_id = o.item_id);
+ON (i.item_id = r.item_id);
 
-CREATE VIEW MOTHERBOARD 
+CREATE VIEW MOTHERBOARD_VIEW 
 AS SELECT m.*, i.item_id AS base_item_id, i.item_name, i.vendor_id, i.item_desc, i.item_cat AS base_item_cat, i.item_price 
 FROM item i INNER JOIN motherboard m
 ON (i.item_id = m.item_id);
 
-CREATE VIEW VIDEOCARD 
+CREATE VIEW VIDEOCARD_VIEW 
 AS SELECT v.*, i.item_id AS base_item_id, i.item_name, i.vendor_id, i.item_desc, i.item_cat AS base_item_cat, i.item_price 
 FROM item i INNER JOIN video_card v
 ON (i.item_id = v.item_id);
@@ -45,7 +45,7 @@ FROM customer AS c
 INNER JOIN item as i
 	ON c.item_name = i.item_name AND c.item_price = i.item_price 
 INNER JOIN address as a
-	ON c.street_number = a.street_number AND c.apt_no = a.apt_no AND c.street_name = a.street_name AND c.city = a.city AND c.country_id = a.country_id
+	ON c.street_number = a.street_number AND c.apt_no = a.apt_no AND c.street_name = a.street_name AND c.city = a.city AND c.country_id = a.country_id;
 
     
 CREATE VIEW VendorView AS
